1f965339a8776b4f454fca994bf78885a8e4e533
c.wang <c.wang2@hi-p.com>
2016-06-21 18:32:41 +0800

    FPII-2108: Elevation of Privilege Vulnerability in Kernel File System (Device Specific) CVE-2016-3775 ANDROID-28588279
    An elevation of privilege vulnerability in the kernel file system could enable a local malicious application to execute arbitrary code within the context of the kernel. This issue is rated as Critical due to the possibility of a local permanent device compromise, which may require reflashing the operating system to repair the device.
    
    Additional Technical Details: 
    
    ANDROID-28588279
    
    There is no validation of the kiocb parameter passed to the aio_setup_vector function.
    
    The fix is to designed to add additional bounds checks.
    
    Change-Id: I9fef036df4636ef0f1ef0ae3613c810b9c445969

--- fs/aio.c
+++ fs/aio.c
@@ -1469,9 +1469,17 @@ out:
 
 static ssize_t aio_setup_single_vector(struct kiocb *kiocb)
 {
+	size_t len = kiocb->ki_nbytes;
+
+	if (len > MAX_RW_COUNT)
+		len = MAX_RW_COUNT;
+
+	if (unlikely(!access_ok(!rw, kiocb->ki_buf, len)))
+                return -EFAULT;
 	kiocb->ki_iovec = &kiocb->ki_inline_vec;
 	kiocb->ki_iovec->iov_base = kiocb->ki_buf;
-	kiocb->ki_iovec->iov_len = kiocb->ki_left;
+	//kiocb->ki_iovec->iov_len = kiocb->ki_left;
+        kiocb->ki_iovec->iov_len = len;
 	kiocb->ki_nr_segs = 1;
 	kiocb->ki_cur_seg = 0;
 	return 0;
