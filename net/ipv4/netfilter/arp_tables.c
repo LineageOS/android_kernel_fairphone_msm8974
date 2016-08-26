363858430232455f0869bcc6fd5b932f598ea78a
Teow Wan Yee <wy.teow@hi-p.com>
2016-08-26 16:53:10 +0800

    FPII-2340: Information disclosure vulnerability in kernel networking subsystem (device specific) CVE-2016-4998 A-29637687
    The IPT_SO_SET_REPLACE setsockopt implementation in the netfilter subsystem,
    in the Linux kernel before 4.6, allows local users to cause a denial of service (out-of-bounds read)
    or possibly obtain sensitive information from kernel heap memory by leveraging in-container root access to
    provide a crafted offset value that leads to crossing a ruleset blob boundary.
    The fix is designed to add additional bounds checks to prevent the information disclosure vulnerability.
    
    Change-Id: Iddb544b809e9fd8ad034207bd3764579f47a5308

--- net/ipv4/netfilter/arp_tables.c
+++ net/ipv4/netfilter/arp_tables.c
@@ -224,7 +224,6 @@ arpt_error(struct sk_buff *skb, const struct xt_action_param *par)
 	if (net_ratelimit())
 		pr_err("arp_tables: error: '%s'\n",
 		       (const char *)par->targinfo);
-
 	return NF_DROP;
 }
 
@@ -470,14 +469,12 @@ static int mark_source_chains(const struct xt_table_info *newinfo,
 	return 1;
 }
 
-static inline int check_entry(const struct arpt_entry *e, const char *name)
+static inline int check_entry(const struct arpt_entry *e)
 {
 	const struct xt_entry_target *t;
 
-	if (!arp_checkentry(&e->arp)) {
-		duprintf("arp_tables: arp check failed %p %s.\n", e, name);
+	if (!arp_checkentry(&e->arp))
 		return -EINVAL;
-	}
 
 	if (e->target_offset + sizeof(struct xt_entry_target) > e->next_offset)
 		return -EINVAL;
@@ -518,9 +515,9 @@ find_check_entry(struct arpt_entry *e, const char *name, unsigned int size)
 	struct xt_target *target;
 	int ret;
 
-	ret = check_entry(e, name);
-	if (ret)
-		return ret;
+    //e->counters.pcnt = xt_percpu_counter_alloc();
+	//if (IS_ERR_VALUE(e->counters.pcnt))
+	//	return -ENOMEM;
 
 	t = arpt_get_target(e);
 	target = xt_request_find_target(NFPROTO_ARP, t->u.user.name,
@@ -566,7 +563,7 @@ static inline int check_entry_size_and_hooks(struct arpt_entry *e,
 					     unsigned int valid_hooks)
 {
 	unsigned int h;
-
+    int err;
 	if ((unsigned long)e % __alignof__(struct arpt_entry) != 0 ||
 	    (unsigned char *)e + sizeof(struct arpt_entry) >= limit) {
 		duprintf("Bad offset %p\n", e);
@@ -579,6 +576,9 @@ static inline int check_entry_size_and_hooks(struct arpt_entry *e,
 			 e, e->next_offset);
 		return -EINVAL;
 	}
+    err = check_entry(e);
+    if (err)
+        return err;
 
 	/* Check hooks & underflows */
 	for (h = 0; h < NF_ARP_NUMHOOKS; h++) {
@@ -1235,7 +1235,7 @@ check_compat_entry_size_and_hooks(struct compat_arpt_entry *e,
 	}
 
 	/* For purposes of check_entry casting the compat entry is fine */
-	ret = check_entry((struct arpt_entry *)e, name);
+    ret = check_entry((struct arpt_entry *)e);
 	if (ret)
 		return ret;
 
