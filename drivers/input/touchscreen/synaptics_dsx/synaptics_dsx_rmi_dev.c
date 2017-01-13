0f84cb183c8be3f2c1ccd8e110f206eea15f754b
Andrew Chant <achant@google.com>
2017-01-13 13:33:57 -0800

    input: synaptics_dsx: protect tmpbuf allocation.
    Protect tmpbuf from concurrent access by mutex.
    
    FPIIM-385
    
    BUG: 33555878
    BUG: 33002026
    Change-Id: Ia986a34647d5825946594ea17a5cd6fa0abb115f
    Signed-off-by: Andrew Chant <achant@google.com>

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_rmi_dev.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_rmi_dev.c
@@ -462,16 +462,26 @@ static ssize_t rmidev_read(struct file *filp, char __user *buf,
 		return -EBADF;
 	}
 
-	if (count == 0)
-		return 0;
+	mutex_lock(&(dev_data->file_mutex));
 
+	if (*f_pos > REG_ADDR_LIMIT) {
+		retval = -EFAULT;
+		goto unlock;
+	}
+	
 	if (count > (REG_ADDR_LIMIT - *f_pos))
 		count = REG_ADDR_LIMIT - *f_pos;
 
+	if (count == 0) {
+		retval = 0;
+		goto unlock;
+	}
+
     tmpbuf = kzalloc(count + 1, GFP_KERNEL);
-	if (!tmpbuf)
-		return -ENOMEM;
-	mutex_lock(&(dev_data->file_mutex));
+	if (!tmpbuf) {
+		retval = -ENOMEM;
+		goto unlock;
+	}
 
 	retval = synaptics_rmi4_reg_read(rmidev->rmi4_data,
 			*f_pos,
@@ -486,8 +496,9 @@ static ssize_t rmidev_read(struct file *filp, char __user *buf,
 		*f_pos += retval;
 
 clean_up:
-	mutex_unlock(&(dev_data->file_mutex));
     kfree(tmpbuf);
+unlock:
+		mutex_unlock(&(dev_data->file_mutex));
 	return retval;
 
 }
@@ -511,22 +522,30 @@ static ssize_t rmidev_write(struct file *filp, const char __user *buf,
 		pr_err("%s: Pointer of char device data is invalid", __func__);
 		return -EBADF;
 	}
-
-	if (count == 0)
-		return 0;
+	mutex_lock(&(dev_data->file_mutex));
+	if (*f_pos > REG_ADDR_LIMIT) {
+		retval = -EFAULT;
+		goto unlock;
+	}
 
 	if (count > (REG_ADDR_LIMIT - *f_pos))
 		count = REG_ADDR_LIMIT - *f_pos;
 
-    tmpbuf = kzalloc(count + 1, GFP_KERNEL);
-	if (!tmpbuf)
-		return -ENOMEM;
+	if (count == 0) {
+		retval = 0;
+		goto unlock;
+	}
+
+	tmpbuf = kzalloc(count + 1, GFP_KERNEL);
+	if (!tmpbuf) {
+		retval = -ENOMEM;
+		goto unlock;
+	}
 
 	if (copy_from_user(tmpbuf, buf, count)) {
-		kfree(tmpbuf);
- 		return -EFAULT;
+		retval = -EFAULT;
+		goto cleanup;
 	}
-	mutex_lock(&(dev_data->file_mutex));
 
 	retval = synaptics_rmi4_reg_write(rmidev->rmi4_data,
 			*f_pos,
@@ -535,8 +554,10 @@ static ssize_t rmidev_write(struct file *filp, const char __user *buf,
 	if (retval >= 0)
 		*f_pos += retval;
 
+cleanup:
+	kfree(tmpbuf);
+unlock:
 	mutex_unlock(&(dev_data->file_mutex));
-    kfree(tmpbuf);
 	return retval;
 }
 
