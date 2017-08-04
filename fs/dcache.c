e5d1a46dd11df5574c4d627c971979b88ee8a994
gregkh@linuxfoundation.org <gregkh@linuxfoundation.org>
2017-08-04 16:16:18 -0700

    dentry name snapshots
    FPIIM-2130
    
    Backport for FP2 kernel:
    
    ------------------
    
    From: Ben Hutchings <ben@decadent.org.uk>
    X-Patchwork-Id: 9908651
    
    3.16.47-rc1 review patch.  If anyone has any objections, please let me
    know.
    
    ------------------
    
    From: Al Viro <viro@zeniv.linux.org.uk>
    
    commit 49d31c2f389acfe83417083e1208422b4091cd9e upstream.
    
    take_dentry_name_snapshot() takes a safe snapshot of dentry name;
    if the name is a short one, it gets copied into caller-supplied
    structure, otherwise an extra reference to external name is grabbed
    (those are never modified).  In either case the pointer to stable
    string is stored into the same structure.
    
    dentry must be held by the caller of take_dentry_name_snapshot(),
    but may be freely dropped afterwards - the snapshot will stay
    until destroyed by release_dentry_name_snapshot().
    
    Intended use:
        struct name_snapshot s;
    
        take_dentry_name_snapshot(&s, dentry);
        ...
        access s.name
        ...
        release_dentry_name_snapshot(&s);
    
    Replaces fsnotify_oldname_...(), gets used in fsnotify to obtain the
    name
    to pass down with event.
    
    Change-Id: Icfb47ce799e191322e1c8bde6b091b7c3b44b1a4
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    [carnil: backport 4.9: adjust context]
    [bwh: Backported to 3.16:
     - External names are not ref-counted, so copy them
     - Adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- fs/dcache.c
+++ fs/dcache.c
@@ -208,6 +208,43 @@ static void __d_free(struct rcu_head *head)
 	kmem_cache_free(dentry_cache, dentry); 
 }
 
+void take_dentry_name_snapshot(struct name_snapshot *name, struct dentry *dentry)
+{
+	spin_lock(&dentry->d_lock);
+	if (unlikely(dname_external(dentry))) {
+		u32 len;
+		char *p;
+
+		for (;;) {
+			len = dentry->d_name.len;
+			spin_unlock(&dentry->d_lock);
+
+			p = kmalloc(len + 1, GFP_KERNEL | __GFP_NOFAIL);
+
+			spin_lock(&dentry->d_lock);
+			if (dentry->d_name.len <= len)
+				break;
+			kfree(p);
+		}
+		memcpy(p, dentry->d_name.name, dentry->d_name.len + 1);
+		spin_unlock(&dentry->d_lock);
+
+		name->name = p;
+	} else {
+		memcpy(name->inline_name, dentry->d_iname, DNAME_INLINE_LEN);
+		spin_unlock(&dentry->d_lock);
+		name->name = name->inline_name;
+	}
+}
+EXPORT_SYMBOL(take_dentry_name_snapshot);
+
+void release_dentry_name_snapshot(struct name_snapshot *name)
+{
+	if (unlikely(name->name != name->inline_name))
+		kfree(name->name);
+}
+EXPORT_SYMBOL(release_dentry_name_snapshot);
+
 /*
  * no locks, please.
  */
@@ -3093,3 +3130,4 @@ void __init vfs_caches_init(unsigned long mempages)
 	bdev_cache_init();
 	chrdev_init();
 }
+
