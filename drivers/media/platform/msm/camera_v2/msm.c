b5768a65ede068d1c01a436f6cd6414913657fc5
Trishansh Bhardwaj <tbhardwa@codeaurora.org>
2017-10-09 17:17:16 +0530

    msm: camera: Return -NOTTY on invalid ioctl command.
    Check validity of command before processing.
    
    Change-Id: Icc5c57eac999b7c40fbb9505b2b88745167adc66
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -558,6 +558,15 @@ static long msm_private_ioctl(struct file *file, void *fh,
 	unsigned int stream_id;
 	unsigned long spin_flags = 0;
 
+	switch (cmd) {
+	case MSM_CAM_V4L2_IOCTL_NOTIFY:
+	case MSM_CAM_V4L2_IOCTL_CMD_ACK:
+	case MSM_CAM_V4L2_IOCTL_NOTIFY_ERROR:
+		break;
+	default:
+		return -ENOTTY;
+	}
+
 	event_data = (struct msm_v4l2_event_data *)
 		((struct v4l2_event *)arg)->u.data;
 
